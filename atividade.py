03ec757f30a4a5ddc4274c823524f2c1cf6f8c00
caua theodoro <caua.t.souza6@aluno.senai.br>
2025-11-25 14:29:30 -0300

    cadastrar personagem

--- atividade.py
+++ atividade.py
@@ -1,24 +1,33 @@
-Herois = {
-    "sh001":{
-    "nome": "homem-aranha",
-    "identidade": "piter parck",
-    "nivel de poder": [200],
-    "universo": "marvel",
-}} 
+cadastro = {}
 
 while True:
-    print("\n1- cadrastar")  
-    print("2- listar")  
-    print("3- atualizar")  
-    print("4- remover")  
-    print("5- sair") 
-    opcao = input("escolha uma opção:")
-    if opcao == "1":
-        codigo  = input(f"seu personagem é Heroi ou vilao:")
-        if nome  in opcao:
-            print("essa opcao já existie!")
-            nome = input("nome do personagens:")
-            identidade = input("qual é a sua indentidade secreta:")
-            nivel = input("pontos de vida do personagens:")
-            universo = input("qual universo:")
-            [codigo] = {
+    print("\n===== MENU =====")
+    print("1 - Cadastrar personagem")
+    print("2 - Listar personagens")
+    print("3 - Atualizar personagem")
+    print("4 - Remover personagem")
+    print("5 - Sair")
+    
+    opcao = input("Escolha uma opção: ")
+
+if opcao == "1":
+        codigo = input("Código do personagem (ex: p001): ")
+
+        if codigo in cadastro:
+            print("Este código já existe!")
+        else:
+            nome = input("Nome: ")
+            classe = input("Classe: ")
+            nivel = input("Nível: ")
+            vida = input("Vida: ")
+            mana = input("Mana: ")
+
+            cadastro[codigo] = {
+                "nome": nome,
+                "classe": classe,
+                "nivel": nivel,
+                "vida": vida,
+                "mana": mana
+            }
+
+            print("Personagem cadastrado com sucesso!")
